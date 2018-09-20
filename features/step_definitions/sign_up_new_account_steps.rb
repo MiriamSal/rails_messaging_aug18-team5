b01ddcd1a0f720532cce8200b586bf3ca2b80c6b
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-20 10:59:50 +0200

    Modifying sign up steps giving specific names to variables

--- features/step_definitions/sign_up_new_account_steps.rb
+++ features/step_definitions/sign_up_new_account_steps.rb
@@ -8,16 +8,16 @@ Given("I am on the landing page") do
   visit root_path
 end
   
-When("I click on {string}") do |string|
-  click_on string 
+When("I click on {string}") do |link|
+  click_on link 
 end
 
-And("I fill in {string} with {string}") do |string1, string2|
-  fill_in(string1, with: string2)
+And("I fill in {string} with {string}") do |name, content|
+  fill_in(name, with: content)
 end
   
-When("i click on {string}") do |string|
-  click_on string
+When("i click on {string}") do |button|
+  click_on button
 end
   
 Then("I should see a message {string}") do |message|
